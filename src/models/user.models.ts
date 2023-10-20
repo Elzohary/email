2348d76b214099df7977d562fb74aaedf2382822
MarkTinega <martinozas@gmail.com>
2023-10-20 16:58:32 +0300

    committed user models module

---
+++ src/models/user.models.ts
@@ -0,0 +1,31 @@
+import { ObjectId, MongoClient, Document } from 'mongodb';
+
+export interface User {
+  _id?: ObjectId;
+  name: string;
+  email: string;
+  password: string;
+}
+
+export class UserModel {
+  private static collectionName = 'users';
+
+  static async createUser(user: User): Promise<User> {
+    const client = await MongoClient.connect('DB_URI');
+    const db = client.db('users-db');
+    const usersCollection = db.collection<User>(UserModel.collectionName);
+    user._id = new ObjectId();
+    await usersCollection.insertOne(user);
+    client.close();
+    return user;
+  }
+
+  static async getUserById(id: string): Promise<User | null> {
+    const client = await MongoClient.connect('DB_URI');
+    const db = client.db('users-db');
+    const usersCollection = db.collection<User>(UserModel.collectionName);
+    const user = await usersCollection.findOne({ _id: new ObjectId(id) });
+    client.close();
+    return user;
+  }
+}
